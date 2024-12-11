55985a60009e33db021d3e39089a4aa8a5cf1f3b
Ghiorghi777 <goncargeorgij9@gmail.com>
2024-12-11 10:18:23 +0200

    Add files via upload

---
+++ ConsoleApp32/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp32
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первую букву:");
+            char letter = Char.ToLower(Console.ReadKey().KeyChar);
+            Console.WriteLine();
+            Console.ReadKey();
+
+            switch (letter)
+            {
+                case 'ф':
+                    Console.WriteLine("Физика");
+                    break;
+                case 'м':
+                    Console.WriteLine("Математика");
+                    break;
+                case 'и':
+                    Console.WriteLine("История");
+                    break;
+                case 'г':
+                    Console.WriteLine("География");
+                    break;
+                case 'б':
+                    Console.WriteLine("Биология");
+                    break;
+                default:
+                    Console.WriteLine("Неизвестная буква");
+                    break;
+            }
+        }
+    }
+}
